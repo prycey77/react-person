523fb3187877b3a2060b7e16d77d3249d02953a4
Richard Pryce <rich_pryce@yahoo.co.uk>
2021-01-24 22:56:51 +0000

    renders a ul element

---
+++ person-app/src/PersonList.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PersonList from './PersonList';
+
+describe('PersonList', () => {
+    it('', () => {
+        const personListWrapper = shallow(<PersonList />);
+        const peopleListUls = personListWrapper.find('ul');
+        expect(peopleListUls).toHaveLength(1);
+    })
+})
